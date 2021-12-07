28c41e9b3e7c618edd837ce100005f8e74e3794c
charef08 <b.charef@outlook.fr>
2021-12-07 02:20:35 +0100

    working to submit a form

--- App.py
+++ App.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index(): 
     return render_template('index.html')
+
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    Algo = request.form['algo']
+    methode = request.form['methode']
+    key = request.key['key']
+    return 'algo : '+ algo+'methode : '+methode 
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
