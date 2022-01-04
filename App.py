d4fdf885381033983d99ee1f63049e0fca8d1ff5
charef08 <b.charef@outlook.fr>
2022-01-04 09:37:20 +0100

    fixed the js problem

--- App.py
+++ App.py
@@ -7,13 +7,45 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/process', methods=['GET', 'POST'])
-def process():
-    Algo = request.form['algo']
-    methode = request.form['methode']
-    key = request.key['key']
-    return 'algo : '+ algo+'methode : '+methode 
+@app.route('/process_form', methods=['GET', 'POST'])
+def process_form():
+    data = request.get_data()
+    print('i got this : ', data)
+    print('success')
+    return render_template('example.html')
 
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+
+
+# letter_freq = {
+#     'a':0.07636,
+#     'b':0.00901,
+#     'c':0.0326,
+#     'd':0.03669,
+#     'e':0.14715,
+#     'f':0.01066,
+#     'g':0.00866,
+#     'h':0.00737,
+#     'i':0.07529,
+#     'j':0.00613,
+#     'k':0.00074,
+#     'l':0.05456,
+#     'm':0.02968,
+#     'n':0.07095,
+#     'o':0.05796,
+#     'p':0.02521,
+#     'q':0.01362,
+#     'r':0.06693,
+#     's':0.07948,
+#     't':0.07244,
+#     'u':0.06311,
+#     'v':0.01838,
+#     'w':0.00049,
+#     'x':0.00427,
+#     'y':0.00128,
+#     'z':0.00326
+# }
---
+++ static/js/functions.js
@@ -0,0 +1,42 @@
+console.log('algorithm')
+
+function getText(){
+    const algorithm = 'vigenere'
+    const text = 'test'
+    const key = 'psswd'
+    const metd = 'encrypt'
+
+    console.log(algorithm)
+
+    (async () => {
+        const rawResponse = await fetch('http://127.0.0.1:5000/test', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            algorithm = algorithm,
+            text = text,
+            key = key,
+            metd = metd
+            })
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+    })();
+
+    // $.ajax({
+    //     url: '/process_form',
+    //     type: 'POST',
+    //     data: JSON.stringify({
+    //         algorithm = algorithm,
+    //         text = text,
+    //         key = key,
+    //         metd = metd
+    //     }),
+    // });
+} 
+
+document.getElementById('clickMe').addEventListener('click', getText, true);
